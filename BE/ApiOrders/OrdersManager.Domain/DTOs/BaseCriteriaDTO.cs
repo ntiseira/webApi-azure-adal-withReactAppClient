718bd2b335908bf0962472e236425f70d5bfd954
NICOLAS <nicolas.tiseira@globallogic.com>
2018-10-30 00:07:23 -0300

    change properties to evaluate null parameteres in DTO

--- BE/ApiOrders/OrdersManager.Domain/DTOs/BaseCriteriaDTO.cs
+++ BE/ApiOrders/OrdersManager.Domain/DTOs/BaseCriteriaDTO.cs
@@ -10,11 +10,30 @@ namespace OrdersManager.Domain.DTOs
     {
         public int PageNumber { get; set; }
         private string _filter;
-        public string Filter { get { return _filter; } set { _filter = value.Trim(); } }
+        public string Filter
+        {
+            get { return _filter; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    _filter = "";
+                else
+                    _filter = value.Trim();
+            }
+        }
         public bool OrderAsc { get; set; }
-        //public bool Mobile { get; set; }
-        //public DateTime? DateFrom { get; set; }
-        //public DateTime? DateTo { get; set; }
-        public string OrderBy { get; set; }
+
+        private string _orderBy;
+        public string OrderBy
+        {
+            get { return _orderBy; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    _orderBy = "";
+                else
+                    _orderBy = value.Trim();
+            }
+        }
     }
 }
