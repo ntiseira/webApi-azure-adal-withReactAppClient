980248c772ca8211b9a56c21eaeec5b3b919ef36
NICOLAS <nicolas.tiseira@globallogic.com>
2018-11-19 20:50:13 -0300

    blank endpoint fied

--- FE/src/AdalReact/adalConfig.js
+++ FE/src/AdalReact/adalConfig.js
@@ -1,13 +1,13 @@
 import { AuthenticationContext, adalFetch, withAdalLogin, adalGetToken } from 'react-adal';
 
 // Endpoint URL
-export const endpoint = 'https://ntiseirahotmail.onmicrosoft.com/OrdersApi';
+export const endpoint = '';
 
 
 export const adalConfig = {
   instance: 'https://login.microsoftonline.com/',
-tenant: 'dac31651-9c13-49b9-b8aa-fcffadcec04a',
-clientId: '44be3f69-df2f-44d7-9ccc-99f888098369',
+tenant: '',
+clientId: '',
 postLogoutRedirectUri: window.location.origin,
   cacheLocation: 'localStorage',
 
